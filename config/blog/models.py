38c6070e8f9f6ad89ce93affa985be6feec2621c
Zaks Danil <zaksdanil1913@example.com>
2024-07-17 16:29:04 +0300

    realize models for article and category

--- config/blog/models.py
+++ config/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    main_image = models.ImageField(upload_to='blog/images/')
+    preview_image = models.ImageField(upload_to='blog/images/')
+    summary = models.TextField()
+    content = models.TextField()
+    publication_date = models.DateField(auto_now_add=True)
+    categories = models.ManyToManyField(Category)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
